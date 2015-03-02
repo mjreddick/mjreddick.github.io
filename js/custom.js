cccadfff01dae173265236e42663327e67869a5a
Matthew Reddick <matthewjreddick@gmail.com>
2015-03-01 22:44:06 -0800

    add rest of personal content, remove what I can do section, and add new styling

--- js/custom.js
+++ js/custom.js
@@ -46,21 +46,33 @@
 		});
 	});
 
-	//nivo lightbox
-	$('.gallery-item a').nivoLightbox({
-		effect: 'fadeScale',                             // The effect to use when showing the lightbox
-		theme: 'default',                           // The lightbox theme to use
-		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
-		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
-		onInit: function(){},                       // Callback when lightbox has loaded
-		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
-		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
-		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
-		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
-		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
-		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
-		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+	$(function(){
+		$('.img-overlay img').click(function(){
+			$(this).closest('.img-overlay').find('.project-overlay').slideToggle("fast");
+		});
 	});
+
+	$(function(){
+		$('.overlay-title').click(function(){
+			$(this).closest('.img-overlay').find('.project-overlay').slideToggle("fast");
+		});
+	});
+
+	//nivo lightbox
+	// $('.gallery-item a').nivoLightbox({
+	// 	effect: 'fadeScale',                             // The effect to use when showing the lightbox
+	// 	theme: 'default',                           // The lightbox theme to use
+	// 	keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+	// 	clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+	// 	onInit: function(){},                       // Callback when lightbox has loaded
+	// 	beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+	// 	afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+	// 	beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+	// 	afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+	// 	onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+	// 	onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+	// 	errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+	// });
 
 	jQuery('.appear').appear();
 	jQuery(".appear").on("appear", function(data) {
